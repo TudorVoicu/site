82e0b760a500c453733fff7fae66837bbb7e54d7
TudorVoicu <tudor.voicu99@gmail.com>
2025-01-30 23:39:58 +0100

    oopsie

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Change to match your repo name
+  base: '/site/', // Change to match your repo name
 });
